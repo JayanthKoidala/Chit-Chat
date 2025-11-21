f30486a6e64d084ea3b1797131b4aebde428a03d
Jayanthkoidala <jayanthkoidala17@gmail.com>
2025-11-21 21:17:01 +0530

    Updated Middlewares

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,22 +1,31 @@
-import jwt from "jsonwebtoken";
+import { jwt } from "jsonwebtoken";
 import User from "../models/User.js";
-import { ENV } from "../lib/env.js";
+import asyncHandler from "express-async-handler";
 
-export const protectRoute = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ message: "Unauthorized - No token provided" });
+export const protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-    const decoded = jwt.verify(token, ENV.JWT_SECRET);
-    if (!decoded) return res.status(401).json({ message: "Unauthorized - Invalid token" });
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return res.status(404).json({ message: "User not found" });
+      //decodes token id
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log("Error in protectRoute middleware:", error);
-    res.status(500).json({ message: "Internal server error" });
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
   }
-};
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
